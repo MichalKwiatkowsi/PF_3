32ca22e8cff2949a0fed15642def2892c7967fb5
Michał Kwiatkowski <92642665+MichalKwiatkowsi@users.noreply.github.com>
2023-05-26 20:53:57 +0200

    Update Fibonacci.java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,22 +1,22 @@
+import java.math.BigInteger;
+
 public class Fibonacci {
-    public static void main(String[] args) {
-        int n = 10;
-        for (int i = 0; i < n; i++) {
-            System.out.println("Fib(" + i + ") = " + fib(i));
+    public static BigInteger fib(int n) {
+        BigInteger a = BigInteger.ZERO;
+        BigInteger b = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            BigInteger temp = b;
+            b = a.add(b);
+            a = temp;
         }
+
+        return a;
     }
 
-    public static int fib(int n) {
-        if (n < 2) {
-            return n;
-        }
-        int a = 0;
-        int b = 1;
-        for (int i = 2; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
+    public static void main(String[] args) {
+        int n = 10000;
+        BigInteger result = fib(n);
+        System.out.println("Fib(" + n + "): " + result);
     }
 }
